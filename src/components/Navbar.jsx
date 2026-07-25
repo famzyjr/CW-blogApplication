fb8e03da8c779a38f5541c42f87ff95b9463c4c8
famzyjr <famuyiwaahmed132@gmail.com>
2026-07-25 15:42:56 +0100

    added a navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="s">
+        <nav>
+          <h2>IBlog</h2>
+          <ul>
+            <li>Home</li>
+            <li>Create blog</li>
+          </ul>
+        </nav>
+        <hr></hr>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
