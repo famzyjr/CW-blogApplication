4814ac8d2c35183237274b74b44bb4e78c8cc253
famzyjr <famuyiwaahmed132@gmail.com>
2026-07-28 16:18:25 +0100

    fix-UI : updated blogList style by adding a responsive card  and hover effect

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,24 +1,29 @@
 import { Link } from "react-router-dom";
 
-const BlogList = ({blog, title}) =>{
-
-return(
-  <div className="blog-list">
-    <h2>{title}</h2>
- {blog.map((blogs)=> (
-    <div className="blog-preview" key={blogs.id}>
-     <Link to={`/blogs/${blogs.id}`}>
-     <h2>{blogs.title}</h2>
-      <p>written by {blogs.author_name}</p>
-      <p>Time created  {blogs.created_at}</p>
-
-     </Link>
-    </div>
-   
-    ))}
-
-  </div>
-)
-}
+const BlogList = ({ blog, title }) => {
+  return (
+    <>
+      <h2 style={{textAlign:'center'}}>{title}</h2>
+      <div className="blog-list">
+        {blog.map((blogs) => (
+          <div className="card_con">
+            <div className="card" key={blogs.id}>
+              <Link to={`/blogs/${blogs.id}`}>
+                <h2>{blogs.title}</h2>
+                <p>written by {blogs.author_name}</p>
+                <p>Time created {blogs.created_at}</p>
+                <div class="overlay">
+                  <Link href="details.html" class="btn">
+                    Read More
+                  </Link>
+                </div>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default BlogList;
