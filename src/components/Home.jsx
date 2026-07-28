53c41f972d17e08797ee62bcebbd72718b9d4beb
famzyjr <famuyiwaahmed132@gmail.com>
2026-07-28 23:51:35 +0100

    improvement: created a new component (Loading-state)  refactored the Loading state

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BlogList from "./BlogList";
-import useFetch from '../../hooks/useFetch'
+import useFetch from '../../hooks/useFetch';
+import LoadingState from './LoadingState';
 const endpoint = 'https://cw-blog-backend.onrender.com'
 const Home = () => {
 const {data:blogs, ispending, error} = useFetch(`${endpoint}/api/blogs`)
@@ -8,7 +9,7 @@ const {data:blogs, ispending, error} = useFetch(`${endpoint}/api/blogs`)
   return (
     <div className="home">
       {error && <div>{error}</div>}
-      {ispending && <div>Loading...</div>}
+      {ispending && <div><LoadingState/></div>}
       {/* child component  */}
       {blogs && <BlogList blog={blogs} title="All Blogs!"/>}
   
---
+++ src/components/LoadingState.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ThreeDot } from 'react-loading-indicators'
+const LoadingState = () => {
+  return (
+    <div className='loading_state'>
+     <ThreeDot variant="bounce" color="#0b0b0b" size="large" text="" textColor="" />
+    </div>
+  )
+}
+
+export default LoadingState
