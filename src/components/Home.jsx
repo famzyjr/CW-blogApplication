75d621a722e020c8560a54faad71c8d7e640049d
famzyjr <famuyiwaahmed132@gmail.com>
2026-08-09 22:59:32 +0100

    removed console,log from home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import LoadingState from './LoadingState';
 const endpoint = 'https://cw-blog-backend.onrender.com'
 const Home = () => {
 const {data:blogs, ispending, error} = useFetch(`${endpoint}/api/blogs`)
-  console.log(blogs);
+
   return (
     <div className="home">
       {error && <div className='errors'>{error}</div>}
