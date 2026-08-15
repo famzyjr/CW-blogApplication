6eadbdd30ef6ae5e8e2803c456daf353bedb9922
famzyjr <famuyiwaahmed132@gmail.com>
2026-08-15 18:20:24 +0100

    add hover labels to icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { User } from "lucide-react";
 const Navbar = () => {
   return (
     <>
       <div className="s">
         <nav>
-         <Link to='/'> <h2>IBlog</h2></Link>
+          <Link to="/">
+            {" "}
+            <h2>IBlog</h2>
+          </Link>
           <ul>
-            <Link to="/blogs">
-            Blogs
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/login">
+             <div className="icon-container">
+                <User className="icon" size={35} />
+              <span className="icon_context">user profile</span>
+             </div>
             </Link>
-             <Link to="/login">
-             Login
+            <Link className="create-btn" to="/createblogs">
+              Create blog
             </Link>
-            <Link className="create-btn" to="/createblogs">Create blog</Link>
           </ul>
         </nav>
         <hr></hr>
