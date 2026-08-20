e8382ebffdf38c607eaef387acbbd9134d96400d
famzyjr <famuyiwaahmed132@gmail.com>
2026-08-20 02:48:53 +0100

    added loading skeleton to the bookmark page

--- src/pages/Bookmarked.jsx
+++ src/pages/Bookmarked.jsx
@@ -1,19 +1,23 @@
 import useFetch from "../../hooks/useFetch";
 import BlogList from "../components/BlogList";
+
 const Bookmarked = () => {
-  const {
-    data:blog,
-  } = useFetch("https://cw-blog-backend.onrender.com/api/blogs");
+  
+  const { data: blog,loading,ispending} = useFetch(
+    "https://cw-blog-backend.onrender.com/api/blogs",
+  );
   const Saved = localStorage.getItem("Bookmarks");
   const BookmarkedIds = Saved ? JSON.parse(Saved) : [];
-const BookmarkedBlogs = blog?.filter((blogs) =>
-  BookmarkedIds.includes(blogs.id)
-);
-  return <div>
-    <BlogList blog={BookmarkedBlogs} title='Your bookmarks'/>
-     
+  const BookmarkedBlogs = blog?.filter((blogs) =>
+    BookmarkedIds.includes(blogs.id),
+  );
+  return (
+    <div>
+       <BlogList blog={BookmarkedBlogs} loading={ispending} title="Your bookmarks" />
+  
     
-  </div>;
+    </div>
+  );
 };
 
 export default Bookmarked;
